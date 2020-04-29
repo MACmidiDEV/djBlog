7ce86ec22b5ac1c5cc2cb70d9555b2aef6a3d807
MACmidiDev <macmidi.webdesigns@gmail.com>
2020-04-29 11:53:34 -0400

    deploy

--- blog/settings.py
+++ blog/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = 'dj-blog-webapp.herokuapp.com'
+ALLOWED_HOSTS = ['dj-blog-webapp.herokuapp.com']
 
 
 # Application definition
